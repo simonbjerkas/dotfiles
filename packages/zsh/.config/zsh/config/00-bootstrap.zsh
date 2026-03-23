6ea26c7d9fdfcaca079aa8cba393f5083a71bf73
simonbjerkas <simon.bjerkas10@gmail.com>
2026-03-23 12:34:02 +0100

    refactor(zsh): completed the zenful workflow. integrated fzf, specified history and added some useful plugins

--- packages/zsh/.config/zsh/config/00-bootstrap.zsh
+++ packages/zsh/.config/zsh/config/00-bootstrap.zsh
@@ -1,8 +1,25 @@
 [[ "$OSTYPE" == darwin* ]] && OS="macos" || OS="linux"
-export OS
 
 if [[ -x "/opt/homebrew/bin/brew" ]]; then
   eval "$(/opt/homebrew/bin/brew shellenv)"
 elif [[ -x "/usr/local/bin/brew" ]]; then
   eval "$(/usr/local/bin/brew shellenv)"
 fi
+
+# Keybindings
+bindkey -e
+bindkey '^p' history-search-backward
+bindkey '^n' history-search-forward
+
+# History
+HISTSIZE=5000
+HISTFILE="${ZDOTDIR}/.zsh_history"
+SAVEHIST=$HISTSIZE
+HISTDUP=erase
+setopt appendhistory
+setopt sharehistory
+setopt hist_ignore_space
+setopt hist_ignore_all_dups
+setopt hist_save_no_dups
+setopt hist_ignore_dups
+setopt hist_find_no_dups
--- packages/zsh/.config/zsh/config/30-plugins.zsh
+++ packages/zsh/.config/zsh/config/30-plugins.zsh
@@ -4,10 +4,18 @@ if [[ "$OS" == "macos" ]]; then
   zinit light romkatv/powerlevel10k
 fi
 
+# Add in snippets
+zinit snippet OMZP::command-not-found
+zinit snippet OMZP::git
+
 # zsh plugins
+zinit light MichaelAquilina/zsh-you-should-use
+zinit light Aloxaf/fzf-tab
 zinit light zsh-users/zsh-completions
 zinit light zsh-users/zsh-autosuggestions
 zinit light zsh-users/zsh-syntax-highlighting
 
 # Load completions
-autoload -U compinit && compinit
+autoload -Uz compinit && compinit
+
+zinit cdreplay -q
